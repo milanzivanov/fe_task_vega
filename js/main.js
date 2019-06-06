337f606745982dfe753a0a16066572d5ac32b350
milan.zivanov <milan.zivanov@gmail.com>
2019-06-06 21:04:24 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+$('#right-arrow').on('click',function() {
+    var currentSlide = currentSlide = $('.slide.active');
+    var nextSlide = currentSlide.next();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    nextSlide.fadeIn(300).addClass('active');
+
+    if(nextSlide.length === 0 ) {
+        $('.slide').first().fadeIn(300).addClass('active');
+    }
+});
+
+$('#left-arrow').on('click',function() {
+    var currentSlide = currentSlide = $('.slide.active');
+    var prevSlide = currentSlide.prev();
+
+    currentSlide.fadeOut(300).removeClass('active');
+    prevSlide.fadeIn(300).addClass('active');
+
+    if(prevSlide.length === 0 ) {
+        $('.slide').last().fadeIn(300).addClass('active');
+    }
+});
+
+
+
