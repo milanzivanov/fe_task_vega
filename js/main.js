954d8bd67a50d002f593b5729a06ca91b867648c
milan.zivanov <milan.zivanov@gmail.com>
2019-06-07 17:40:07 +0200

    transforming png icons to base64 that can used in pictures element

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
 'use strict'
 
-$('#right-arrow').on('click',function() {
-    var currentSlide = currentSlide = $('.slide.active');
-    var nextSlide = currentSlide.next();
+$(document).ready(function(){
 
-    currentSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
 
-    if(nextSlide.length === 0 ) {
-        $('.slide').first().fadeIn(300).addClass('active');
-    }
+    setInterval(function(){
+
+        var currentSlide = currentSlide = $('.slide.active');
+        var nextSlide = currentSlide.next();
+    
+        currentSlide.fadeOut(300).removeClass('active');
+        nextSlide.fadeIn(300).addClass('active');
+    
+        if(nextSlide.length === 0 ) {
+            $('.slide').first().fadeIn(300).addClass('active');
+        }
+
+    }, 10000);
+
+    $('#right-arrow').on('click',function() {
+        var currentSlide = currentSlide = $('.slide.active');
+        var nextSlide = currentSlide.next();
+    
+        currentSlide.fadeOut(300).removeClass('active');
+        nextSlide.fadeIn(300).addClass('active');
+    
+        if(nextSlide.length === 0 ) {
+            $('.slide').first().fadeIn(300).addClass('active');
+        }
+    });
+    
+    
+    $('#left-arrow').on('click',function() {
+        var currentSlide = currentSlide = $('.slide.active');
+        var prevSlide = currentSlide.prev();
+    
+        currentSlide.fadeOut(300).removeClass('active');
+        prevSlide.fadeIn(300).addClass('active');
+    
+        if(prevSlide.length === 0 ) {
+            $('.slide').last().fadeIn(300).addClass('active');
+        }
+    });
+
 });
 
-$('#left-arrow').on('click',function() {
-    var currentSlide = currentSlide = $('.slide.active');
-    var prevSlide = currentSlide.prev();
-
-    currentSlide.fadeOut(300).removeClass('active');
-    prevSlide.fadeIn(300).addClass('active');
-
-    if(prevSlide.length === 0 ) {
-        $('.slide').last().fadeIn(300).addClass('active');
-    }
-});
 
 
 
