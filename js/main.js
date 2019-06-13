c276dd9fc1c8828bea06f872c288653bcc336064
milan.zivanov <milan.zivanov@gmail.com>
2019-06-13 23:03:48 +0200

    tabs classess box js

--- js/main.js
+++ js/main.js
@@ -44,6 +44,28 @@ $(document).ready(function(){
 
 });
 
+// tab
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("service-box");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("service-tabs_tab");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
+  }
+
 
 
 
